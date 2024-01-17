f14aa14b5f3e5f106366119f01f3b3190674343f
Isaiah Stapleton <istaplet@redhat.com>
2024-01-17 17:49:54 -0500

    Add juphub-utils to base image
    Signed-off-by: Isaiah Stapleton <istaplet@redhat.com>

---
+++ containers/testing/base/juphub-utils/reenable_jup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+JUPY_EXTS="@jupyterlab/fileeditor-extension:plugin
+@jupyterlab/filebrowser-extension:widget
+@jupyterlab/filebrowser-extension:browser
+@jupyterlab/filebrowser-extension:download
+@jupyterlab/filebrowser-extension:open-browser-tab
+@jupyterlab/filebrowser-extension:open-with
+@jupyterlab/console-extension:tracker
+@jupyterlab/console-extension:foreign
+@jupyterlab/debugger-extension:main
+@jupyterlab/extensionmanager-extension:plugin
+@jupyterlab/launcher-extension:plugin
+@jupyterlab/apputils-extension:palette
+@jupyterlab/logconsole-extension:plugin
+@jupyterlab/codemirror-extension:commands
+@jupyterlab/inspector-extension:consoles
+@jupyterlab/inspector-extension:inspector
+@jupyterlab/inspector-extension:notebooks
+@jupyterlab/shortcuts-extension:shortcuts"
+
+for ext in ${JUPY_EXTS}; do
+    echo "Enabling: $ext"
+    jupyter labextension enable $ext
+done
+
+echo "resetting /opt/conda/share/jupyter/lab/settings/overrides.json"
+cat <<EOF > /opt/conda/share/jupyter/lab/settings/overrides.json
+{
+    "@jupyterlab/terminal-extension:plugin": {
+        "shutdownOnClose": true
+    },
+    "@jupyterlab/statusbar-extension:plugin": {
+        "visible": false
+    }
+}
+EOF
+
+echo "reload jupyterlab webpage for new setting to take effect"
